50dcc14d830917ff0053be931efaeb7ca505af0a
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 23:14:27 +0630

    [ Added ] CreatePatient Endpoint

--- DotNet8.DoctorAppointmentBookingSystem.Api/Controllers/PatientController.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/Controllers/PatientController.cs
@@ -1,4 +1,5 @@
-﻿using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Patient;
+﻿using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Patient;
+using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Patient;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,13 @@ namespace DotNet8.DoctorAppointmentBookingSystem.Api.Controllers
         public async Task<IActionResult> GetPatients(CancellationToken cancellationToken)
         {
             var result = await _patientService.GetPatientsAsync(cancellationToken);
+            return Content(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePatient([FromBody] CreatePatientDto patientDto, CancellationToken cancellationToken)
+        {
+            var result = await _patientService.AddPatientAsync(patientDto, cancellationToken);
             return Content(result);
         }
     }
