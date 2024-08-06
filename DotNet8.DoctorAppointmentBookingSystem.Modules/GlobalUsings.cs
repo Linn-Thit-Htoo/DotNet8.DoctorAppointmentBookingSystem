cd3de01089061ba5cf5b600d2f5c7e04d3c9f0cb
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:14:31 +0630

    Format Code

---
+++ DotNet8.DoctorAppointmentBookingSystem.Modules/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using DotNet8.DoctorAppointmentBookingSystem.Db.AppDbContextModels;
+global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Appointment;
+global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Utils;
+global using DotNet8.DoctorAppointmentBookingSystem.Extensions;
+global using Microsoft.EntityFrameworkCore;
