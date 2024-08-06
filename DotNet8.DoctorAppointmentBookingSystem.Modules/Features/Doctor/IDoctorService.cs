e42fad7cd2d23cbd882f3868aad62ad82c264c50
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:15:49 +0630

    Format Code

--- DotNet8.DoctorAppointmentBookingSystem.Modules/Features/Doctor/IDoctorService.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Modules/Features/Doctor/IDoctorService.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Doctor;
 
-namespace DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Doctor
+public interface IDoctorService
 {
-    public interface IDoctorService
-    {
-        Task<Result<IEnumerable<DoctorDto>>> GetDoctorListAsync(CancellationToken cancellationToken);
-        Task<Result<DoctorDto>> GetDoctorByIdAsync(string id, CancellationToken cancellationToken);
-        Task<Result<DoctorDto>> AddDoctorAsync(CreateDoctorDto doctorDto, CancellationToken cancellationToken);
-    }
+    Task<Result<IEnumerable<DoctorDto>>> GetDoctorListAsync(CancellationToken cancellationToken);
+    Task<Result<DoctorDto>> GetDoctorByIdAsync(string id, CancellationToken cancellationToken);
+    Task<Result<DoctorDto>> AddDoctorAsync(
+        CreateDoctorDto doctorDto,
+        CancellationToken cancellationToken
+    );
 }
