d67377bda5c1082c60314ec4768673b5e1b5394b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:12:40 +0630

    Format Code

--- DotNet8.DoctorAppointmentBookingSystem.Api/Controllers/PatientController.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/Controllers/PatientController.cs
@@ -1,39 +1,37 @@
-﻿using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Patient;
-using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Patient;
-using Microsoft.AspNetCore.Http;
+﻿namespace DotNet8.DoctorAppointmentBookingSystem.Api.Controllers;
 
-namespace DotNet8.DoctorAppointmentBookingSystem.Api.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class PatientController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PatientController : BaseController
+    private readonly IPatientService _patientService;
+
+    public PatientController(IPatientService patientService)
     {
-        private readonly IPatientService _patientService;
+        _patientService = patientService;
+    }
 
-        public PatientController(IPatientService patientService)
-        {
-            _patientService = patientService;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetPatients(CancellationToken cancellationToken)
+    {
+        var result = await _patientService.GetPatientsAsync(cancellationToken);
+        return Content(result);
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetPatients(CancellationToken cancellationToken)
-        {
-            var result = await _patientService.GetPatientsAsync(cancellationToken);
-            return Content(result);
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPatient(string id, CancellationToken cancellationToken)
+    {
+        var result = await _patientService.GetPatientByIdAsync(id, cancellationToken);
+        return Content(result);
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetPatient(string id, CancellationToken cancellationToken)
-        {
-            var result = await _patientService.GetPatientByIdAsync(id, cancellationToken);
-            return Content(result);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreatePatient([FromBody] CreatePatientDto patientDto, CancellationToken cancellationToken)
-        {
-            var result = await _patientService.AddPatientAsync(patientDto, cancellationToken);
-            return Content(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreatePatient(
+        [FromBody] CreatePatientDto patientDto,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await _patientService.AddPatientAsync(patientDto, cancellationToken);
+        return Content(result);
     }
 }
--- DotNet8.DoctorAppointmentBookingSystem.Api/GlobalUsings.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/GlobalUsings.cs
@@ -2,7 +2,9 @@
 
 global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Appointment;
 global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Doctor;
+global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Patient;
 global using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Appointment;
 global using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Doctor;
+global using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Patient;
 global using Microsoft.AspNetCore.Mvc;
 global using Newtonsoft.Json;
