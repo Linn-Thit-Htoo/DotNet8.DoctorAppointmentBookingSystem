42c6bf83cd778242070bd39ec0d38ce399678636
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:08:03 +0630

    Format Code

--- DotNet8.DoctorAppointmentBookingSystem.Api/Controllers/AppointmentController.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/Controllers/AppointmentController.cs
@@ -1,55 +1,70 @@
 ﻿using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Appointment;
 using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Appointment;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Runtime.CompilerServices;
 
-namespace DotNet8.DoctorAppointmentBookingSystem.Api.Controllers
+namespace DotNet8.DoctorAppointmentBookingSystem.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AppointmentController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AppointmentController : BaseController
+    private readonly IAppointmentService _appointmentService;
+
+    public AppointmentController(IAppointmentService appointmentService)
     {
-        private readonly IAppointmentService _appointmentService;
+        _appointmentService = appointmentService;
+    }
 
-        public AppointmentController(IAppointmentService appointmentService)
-        {
-            _appointmentService = appointmentService;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAppointments(CancellationToken cancellationToken)
+    {
+        var result = await _appointmentService.GetAppointmentsAsync(cancellationToken);
+        return Content(result);
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAppointments(CancellationToken cancellationToken)
-        {
-            var result = await _appointmentService.GetAppointmentsAsync(cancellationToken);
-            return Content(result);
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAppointment(string id, CancellationToken cancellationToken)
+    {
+        var result = await _appointmentService.GetAppointmentByIdAsync(id, cancellationToken);
+        return Content(result);
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetAppointment(string id, CancellationToken cancellationToken)
-        {
-            var result = await _appointmentService.GetAppointmentByIdAsync(id, cancellationToken);
-            return Content(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> BookAppointment(
+        [FromBody] CreateAppointmentDto appointmentDto,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await _appointmentService.BookAppointmentAsync(
+            appointmentDto,
+            cancellationToken
+        );
+        return Content(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> BookAppointment([FromBody] CreateAppointmentDto appointmentDto, CancellationToken cancellationToken)
-        {
-            var result = await _appointmentService.BookAppointmentAsync(appointmentDto, cancellationToken);
-            return Content(result);
-        }
+    [HttpGet("doctor/{doctorId}")]
+    public async Task<IActionResult> GetAppointmentsByDoctorId(
+        string doctorId,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await _appointmentService.GetAppointmentsByDoctorIdAsync(
+            doctorId,
+            cancellationToken
+        );
+        return Content(result);
+    }
 
-        [HttpGet("doctor/{doctorId}")]
-        public async Task<IActionResult> GetAppointmentsByDoctorId(string doctorId, CancellationToken cancellationToken)
-        {
-            var result = await _appointmentService.GetAppointmentsByDoctorIdAsync(doctorId, cancellationToken);
-            return Content(result);
-        }
-
-        [HttpGet("patient/{patientId}")]
-        public async Task<IActionResult> GetAppointmentsByPatientId(string patientId, CancellationToken cancellationToken)
-        {
-            var result = await _appointmentService.GetAppointmentsByPatientIdAsync(patientId, cancellationToken);
-            return Content(result);
-        }
+    [HttpGet("patient/{patientId}")]
+    public async Task<IActionResult> GetAppointmentsByPatientId(
+        string patientId,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await _appointmentService.GetAppointmentsByPatientIdAsync(
+            patientId,
+            cancellationToken
+        );
+        return Content(result);
     }
 }
