6e568f7d5d51529203999de9d458136a68f20e4a
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 21:16:42 +0630

    [ In Progress ] Result Pattern

--- DotNet8.DoctorAppointmentBookingSystem.Api/Utils/Result.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/Utils/Result.cs
@@ -1,6 +1,23 @@
-﻿namespace DotNet8.DoctorAppointmentBookingSystem.Api.Utils
+﻿using DotNet8.DoctorAppointmentBookingSystem.Api.Enums;
+
+namespace DotNet8.DoctorAppointmentBookingSystem.Api.Utils
 {
-    public class Result
+    public class Result<T>
     {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public EnumStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+
+        public static Result<T> Success(string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success) =>
+            new() { Message = message, StatusCode = statusCode, IsSuccess = true };
+
+        public static Result<T> Success(T data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+            => new() { Data = data, Message = message, StatusCode = statusCode, IsSuccess = true };
+
+        public static Result<T> SaveSuccess(string message = "Saving Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+            => Result<T>.Success(message, statusCode);
+        public static Result<T> UpdateSuccess(string message = "Updating Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+            => Result<T>.Success(message, statusCode);
     }
 }
