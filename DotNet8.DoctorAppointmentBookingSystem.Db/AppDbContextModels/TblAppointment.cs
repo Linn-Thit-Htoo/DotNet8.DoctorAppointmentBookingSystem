6915d9b5ccd5aac304c06decda621a04648a3e5a
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:17:07 +0630

    Format Code

--- DotNet8.DoctorAppointmentBookingSystem.Db/AppDbContextModels/TblAppointment.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Db/AppDbContextModels/TblAppointment.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DotNet8.DoctorAppointmentBookingSystem.Db.AppDbContextModels;
+﻿namespace DotNet8.DoctorAppointmentBookingSystem.Db.AppDbContextModels;
 
 public partial class TblAppointment
 {
