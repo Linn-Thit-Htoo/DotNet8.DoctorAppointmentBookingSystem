3f19067c726d5cc51a38a53378584863efebfa81
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:12:59 +0630

    Format Code

--- DotNet8.DoctorAppointmentBookingSystem.Api/DependencyInjection.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/DependencyInjection.cs
@@ -1,34 +1,39 @@
-﻿using DotNet8.DoctorAppointmentBookingSystem.Db.AppDbContextModels;
-using Microsoft.EntityFrameworkCore;
+﻿namespace DotNet8.DoctorAppointmentBookingSystem.Api;
 
-namespace DotNet8.DoctorAppointmentBookingSystem.Api
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddDbContextService(builder)
-                .AddCustomServices();
-            return services;
-        }
+        services.AddDbContextService(builder).AddCustomServices();
+        return services;
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
+    private static IServiceCollection AddDbContextService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        builder.Services.AddDbContext<AppDbContext>(
+            opt =>
             {
                 opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            },
+            ServiceLifetime.Transient,
+            ServiceLifetime.Transient
+        );
 
-            return services;
-        }
+        return services;
+    }
 
-        public static IServiceCollection AddCustomServices(this IServiceCollection services)
-        {
-            services.AddScoped<IDoctorService, DoctorService>();
-            services.AddScoped<IPatientService, PatientService>();
-            services.AddScoped<IAppointmentService, AppointmentService>();
-            return services;
-        }
+    public static IServiceCollection AddCustomServices(this IServiceCollection services)
+    {
+        services.AddScoped<IDoctorService, DoctorService>();
+        services.AddScoped<IPatientService, PatientService>();
+        services.AddScoped<IAppointmentService, AppointmentService>();
+        return services;
     }
 }
--- DotNet8.DoctorAppointmentBookingSystem.Api/GlobalUsings.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
+global using DotNet8.DoctorAppointmentBookingSystem.Db.AppDbContextModels;
 global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Appointment;
 global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Doctor;
 global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Patient;
@@ -7,4 +8,5 @@ global using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Appointment
 global using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Doctor;
 global using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Patient;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
 global using Newtonsoft.Json;
