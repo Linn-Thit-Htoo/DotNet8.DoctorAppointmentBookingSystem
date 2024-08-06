97bd305d3651fe31ae2f0c2ccac27b732efddf70
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 22:42:42 +0630

    [ Added ] PatientDtos

---
+++ DotNet8.DoctorAppointmentBookingSystem.Dtos/Features/Patient/CreatePatientDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Patient
+{
+    public class CreatePatientDto
+    {
+        public string PatientName { get; set; } = null!;
+    }
+}
---
+++ DotNet8.DoctorAppointmentBookingSystem.Dtos/Features/Patient/PatientDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Patient
+{
+    public class PatientDto
+    {
+        public string PatientId { get; set; } = null!;
+
+        public string PatientName { get; set; } = null!;
+    }
+}
