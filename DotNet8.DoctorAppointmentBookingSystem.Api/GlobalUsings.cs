1272979c610c7917b8f2b6676ac7bf556c78a31b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:08:30 +0630

    [ Added ] Global Usings Api

---
+++ DotNet8.DoctorAppointmentBookingSystem.Api/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Appointment;
+global using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Appointment;
+global using Microsoft.AspNetCore.Mvc;
