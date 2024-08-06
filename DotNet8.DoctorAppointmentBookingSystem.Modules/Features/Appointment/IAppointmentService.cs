90ed1d16be8de8031773e2898e937bcafaa50ace
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:14:56 +0630

    Format Code

--- DotNet8.DoctorAppointmentBookingSystem.Modules/Features/Appointment/IAppointmentService.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Modules/Features/Appointment/IAppointmentService.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Appointment;
 
-namespace DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Appointment
+public interface IAppointmentService
 {
-    public interface IAppointmentService
-    {
-        Task<Result<IEnumerable<AppointmentDto>>> GetAppointmentsAsync(CancellationToken cancellationToken);
-        Task<Result<AppointmentDto>> GetAppointmentByIdAsync(string id, CancellationToken cancellationToken);
-        Task<Result<AppointmentDto>> BookAppointmentAsync(CreateAppointmentDto appointment, CancellationToken cancellationToken);
-        Task<Result<IEnumerable<AppointmentDto>>> GetAppointmentsByDoctorIdAsync(string doctorId, CancellationToken cancellationToken);
-        Task<Result<IEnumerable<AppointmentDto>>> GetAppointmentsByPatientIdAsync(string patientId, CancellationToken cancellationToken);
-    }
+    Task<Result<IEnumerable<AppointmentDto>>> GetAppointmentsAsync(
+        CancellationToken cancellationToken
+    );
+    Task<Result<AppointmentDto>> GetAppointmentByIdAsync(
+        string id,
+        CancellationToken cancellationToken
+    );
+    Task<Result<AppointmentDto>> BookAppointmentAsync(
+        CreateAppointmentDto appointment,
+        CancellationToken cancellationToken
+    );
+    Task<Result<IEnumerable<AppointmentDto>>> GetAppointmentsByDoctorIdAsync(
+        string doctorId,
+        CancellationToken cancellationToken
+    );
+    Task<Result<IEnumerable<AppointmentDto>>> GetAppointmentsByPatientIdAsync(
+        string patientId,
+        CancellationToken cancellationToken
+    );
 }
