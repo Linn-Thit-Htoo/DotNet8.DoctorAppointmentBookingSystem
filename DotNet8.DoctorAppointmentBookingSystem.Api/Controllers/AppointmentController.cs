794ae836ac56aa48531d3ba02b859a6a3bbbf20c
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 23:54:19 +0630

    [ Added ] GetAppointments Endpoint

---
+++ DotNet8.DoctorAppointmentBookingSystem.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,25 @@
+﻿using DotNet8.DoctorAppointmentBookingSystem.Modules.Features.Appointment;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.DoctorAppointmentBookingSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : BaseController
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppointments(CancellationToken cancellationToken)
+        {
+            var result = await _appointmentService.GetAppointmentsAsync(cancellationToken);
+            return Content(result);
+        }
+    }
+}
