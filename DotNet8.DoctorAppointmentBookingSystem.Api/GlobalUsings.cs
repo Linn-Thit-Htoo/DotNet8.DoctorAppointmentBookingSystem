9752635776984517e8ba5abda32337855234ec97
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:13:16 +0630

    Format Code

--- DotNet8.DoctorAppointmentBookingSystem.Api/GlobalUsings.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
+global using DotNet8.DoctorAppointmentBookingSystem.Api;
 global using DotNet8.DoctorAppointmentBookingSystem.Db.AppDbContextModels;
 global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Appointment;
 global using DotNet8.DoctorAppointmentBookingSystem.Dtos.Features.Doctor;
--- DotNet8.DoctorAppointmentBookingSystem.Api/Program.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Api/Program.cs
@@ -1,5 +1,3 @@
-using DotNet8.DoctorAppointmentBookingSystem.Api;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
