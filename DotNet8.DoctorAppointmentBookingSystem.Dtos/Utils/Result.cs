0927051981d23a7a0f78d457a0e93c1f5c7b5ef1
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 00:18:56 +0630

    [ Added ] Global Usings

--- DotNet8.DoctorAppointmentBookingSystem.Dtos/Utils/Result.cs
+++ DotNet8.DoctorAppointmentBookingSystem.Dtos/Utils/Result.cs
@@ -1,44 +1,36 @@
-﻿using DotNet8.DoctorAppointmentBookingSystem.Dtos.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.DoctorAppointmentBookingSystem.Dtos.Utils;
 
-namespace DotNet8.DoctorAppointmentBookingSystem.Dtos.Utils
+public class Result<T>
 {
-    public class Result<T>
-    {
-        public T Data { get; set; }
-        public string Message { get; set; }
-        public EnumStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
+    public T Data { get; set; }
+    public string Message { get; set; }
+    public EnumStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; }
 
-        public static Result<T> Success(string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success) =>
-            new() { Message = message, StatusCode = statusCode, IsSuccess = true };
+    public static Result<T> Success(string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success) =>
+        new() { Message = message, StatusCode = statusCode, IsSuccess = true };
 
-        public static Result<T> Success(T data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
-            => new() { Data = data, Message = message, StatusCode = statusCode, IsSuccess = true };
+    public static Result<T> Success(T data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        => new() { Data = data, Message = message, StatusCode = statusCode, IsSuccess = true };
 
-        public static Result<T> SaveSuccess(string message = "Saving Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
-            => Result<T>.Success(message, statusCode);
+    public static Result<T> SaveSuccess(string message = "Saving Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        => Result<T>.Success(message, statusCode);
 
-        public static Result<T> UpdateSuccess(string message = "Updating Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
-            => Result<T>.Success(message, statusCode);
+    public static Result<T> UpdateSuccess(string message = "Updating Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        => Result<T>.Success(message, statusCode);
 
-        public static Result<T> DeleteSuccess(string message = "Deleting Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
-            => Result<T>.Success(message, statusCode);
+    public static Result<T> DeleteSuccess(string message = "Deleting Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        => Result<T>.Success(message, statusCode);
 
-        public static Result<T> Failure(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
-            => new() { Message = message, StatusCode = statusCode, IsSuccess = false };
+    public static Result<T> Failure(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+        => new() { Message = message, StatusCode = statusCode, IsSuccess = false };
 
-        public static Result<T> Failure(Exception ex)
-            => new() { Message = ex.ToString(), StatusCode = EnumStatusCode.InternalServerError, IsSuccess = false };
+    public static Result<T> Failure(Exception ex)
+        => new() { Message = ex.ToString(), StatusCode = EnumStatusCode.InternalServerError, IsSuccess = false };
 
-        public static Result<T> NotFound(string message = "No Data Found.")
-            => Result<T>.Failure(message, EnumStatusCode.NotFound);
+    public static Result<T> NotFound(string message = "No Data Found.")
+        => Result<T>.Failure(message, EnumStatusCode.NotFound);
 
-        public static Result<T> Duplicate(string message = "Duplicate Data.")
-            => Result<T>.Failure(message, EnumStatusCode.Conflict);
-    }
+    public static Result<T> Duplicate(string message = "Duplicate Data.")
+        => Result<T>.Failure(message, EnumStatusCode.Conflict);
 }
